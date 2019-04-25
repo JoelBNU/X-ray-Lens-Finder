643df29b9d658b1f9b34254c8f4572864b043b88
JoelBNU <775739144@qq.com>
2019-04-25 15:33:42 +0800

    Update X-ray Lens Finder.py

--- X-ray Lens Finder.py
+++ X-ray Lens Finder.py
@@ -21,7 +21,7 @@ from pylab import *
 # Defining function to generate the Matrix C.
 # In matrix C, the first column data are a copy of the first column data in matrix B.
 # The second and third columns contain the Y_downfit and Y_upfit data generated according
-# to the first column data and Equation 1 (Eq. 1).
+# to the first column data and Equation 2 (Eq. 2).
 def c_generator(P, x0, y0):
     C = np.zeros((len(B), 3))
     C[:, 0] = B[:, 0] # copy the first column data in B
@@ -29,7 +29,7 @@ def c_generator(P, x0, y0):
     C[:, 2] = sqrt(2 * P * (C[:, 0] + x0)) - y0   # generate Y_upfit
     return C
 
-# Defining function to generate the FF value, corresponding Eq. 2 in the research paper.
+# Defining function to generate the FF value, corresponding Eq. 5 in the research paper.
 def FF_generator(P, x0, y0):
     C = np.zeros((len(B), 3))
     C[:, 0] = B[:, 0]
