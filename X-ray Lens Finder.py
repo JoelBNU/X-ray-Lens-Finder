34691286b8eb089fa1163c6b2eb92f30ed0370dd
JoelBNU <775739144@qq.com>
2019-09-15 13:24:49 +0800

    Update X-ray Lens Finder.py

--- X-ray Lens Finder.py
+++ X-ray Lens Finder.py
@@ -2,6 +2,8 @@
 # -*- coding: UTF-8 -*-
 # @Time    : 2018-11-20
 # @Update  : 2019-04-22
+# @Update  : 2019-08-16
+# @Update  : 2019-09-15 leasted one
 # @Author  : Joel
 # @File    :
 # @Site    :
@@ -29,22 +31,22 @@ def c_generator(P, x0, y0):
     C[:, 2] = sqrt(2 * P * (C[:, 0] + x0)) - y0   # generate Y_upfit
     return C
 
-# Defining function to generate the FF value, corresponding Eq. 5 in the research paper.
-def FF_generator(P, x0, y0):
+# Defining function to generate the PE value, corresponding Eq. 5 in the research paper.
+def PE_generator(P, x0, y0):
     C = np.zeros((len(B), 3))
     C[:, 0] = B[:, 0]
     C[:, 1] = -sqrt(2 * P * (C[:, 0] + x0)) - y0
     C[:, 2] = sqrt(2 * P * (C[:, 0] + x0)) - y0
     Value_Error_matrix = (B[:, 1] - C[:, 1]) ** 2 + (B[:, 2] - C[:, 2]) ** 2
     Value_Error_sum = Value_Error_matrix.sum()
-    FF_value = math.sqrt(Value_Error_sum/float(len(B)))
-    return FF_value
+    PE_value = math.sqrt(Value_Error_sum/(2*float(len(B))))
+    return PE_value
 
 # Defining the PSO program
 class PSO():
     # Program initialization,
-    # input: Particle number, Dimension, Number of iterations, inertia weight, acceleration factors, gfit, dimension_max, coefficient.
-    def __init__(self, Pnum, dim, max_iter,w_start,w_end, c1, c2,gfit,dimension_max, dimension_coefficient):
+    # input: Particle number, Dimension, Number of iterations, inertia weight, acceleration factors.
+    def __init__(self, Pnum, dim, max_iter,w_start,w_end, c1, c2,gfit,gfit_temp,dimension_max, dimension_coefficient):
         self.Pnum = Pnum                                    # Particle number
         self.dim = dim                                      # Dimension
         self.max_iter = max_iter                            # Number of iterations
@@ -58,6 +60,8 @@ class PSO():
         self.gbest = np.zeros((1, self.dim))                # global best position of all particles
         self.pfit = np.zeros(self.Pnum)                     # the initial space for FF value of all particles
         self.gfit = gfit                                    # the initial global FF value in this program
+        self.gfit_temp = gfit_temp                          # the initial global FF temp value for end the optimization
+                                                            # if the fitness-value does not change by a given %.
         self.dimension_max = dimension_max                  # the maximum value of each dimension, it can be manually
                                                             # adjusted according to the data to be analyzed.
         self.dimension_coefficient = dimension_coefficient  # the coefficient to set the maximum velocity according the
@@ -66,10 +70,10 @@ class PSO():
         self.xx0 = 0                                        # the initial x0 value
         self.yy0 = 0                                        # the initial y0 value
 
-    # Defining function to obtain the value of FF.
-    def FF_value_function(self, para1, para2, para3):
-        FF_v = FF_generator(para1, para2, para3)
-        return FF_v
+    # Defining function to obtain the value of PE.
+    def PE_value_function(self, para1, para2, para3):
+        PE_v = PE_generator(para1, para2, para3)
+        return PE_v
 
     # Defining function to generate the C matrix.
     def C_matrix(self):
@@ -91,32 +95,36 @@ class PSO():
             para1 = self.position[i, 0]                   # Obtaining P value of particle i
             para2 = self.position[i, 1]                   # Obtaining x0 value of particle i
             para3 = self.position[i, 2]                   # Obtaining y0 value of particle i
-            FF = self.FF_value_function(para1, para2, para3)  # calculating the FF value of each particle
-            self.pfit[i] = FF                                 # Recording the FF value of each particle
-            if (FF < self.gfit):                       # Determining if it is the current best FF value of all particles
-                self.gfit = FF                         # Updating the global best FF value
+            PE = self.PE_value_function(para1, para2, para3)  # calculating the PE value of each particle
+            self.pfit[i] = PE                                 # Recording the PE value of each particle
+            if (PE < self.gfit):                       # Determining if it is the current best PE value of all particles
+                self.gfit = PE                         # Updating the global best PE value
                 self.gbest = self.position[i]          # Updating the global best position
 
     # Defining function to iterate the particles population
     def iterator(self):
         import random
-        fitness = []  # Recording the global FF value for each iteration
+        PE_fitness = []  # Recording the global PE value for each iteration
+        timer = []    # Recording the calculate time
+        time_cal_start = time.time() #Record the start time
+        judgement = 0
         for t in range(self.max_iter):
+
             for i in range(self.Pnum):
                 para1 = self.position[i, 0]        # Obtaining P value of particle i
                 para2 = self.position[i, 1]        # Obtaining x0 value of particle i
                 para3 = self.position[i, 2]        # Obtaining y0 value of particle i
-                FF = self.FF_value_function(para1, para2, para3)      # calculating the FF value of each particle
-                # Determining if it is the current best FF value of the particle
-                if (FF < self.pfit[i]):
-                    self.pfit[i] = FF              # Updating the best FF value of the particle
+                PE = self.PE_value_function(para1, para2, para3)      # calculating the PE value of each particle
+                # Determining if it is the current best PE value of the particle
+                if (PE < self.pfit[i]):
+                    self.pfit[i] = PE              # Updating the best PE value of the particle
                     self.pbest[i] = self.position[i]  # Updating the best position of the particle
-                    # Determining if it is the current global best FF value of all the particles
+                    # Determining if it is the current global best PE value of all the particles
                     if (self.pfit[i] < self.gfit):
                         self.gbest = self.position[i] # Updating the global best position
-                        self.gfit = self.pfit[i]      # Updating the global best FF value
+                        self.gfit = self.pfit[i]      # Updating the global best PE value
 
-            # Explaining the inertia weight in Eq. 6 in the research paper.
+            # Explaining the inertia weight in Eq. 3 in the research paper.
             # In PSO program, inertia weight (calculate_w) embodies the ability of particles to inherit previous
             # velocity. It can be selected between 0-1, larger weights are good for global search,
             # and smaller weights are good for local search.
@@ -140,23 +148,29 @@ class PSO():
                                           * (self.gbest[j] - self.position[i][j])
                     # If the velocity of any dimension exceeds the maximum velocity in restricted domain, limit it.
                     # the restricted domain was related to the initial velocity of particles population,
-                    # refer to line 87.
+                    # refer to the research paper.
                     if self.velocity[i][j] > 2 * self.dimension_coefficient * self.dimension_max:
                         self.velocity[i][j] = 2 * self.dimension_coefficient * self.dimension_max
                     if self.velocity[i][j] < -2 * self.dimension_coefficient * self.dimension_max:
                         self.velocity[i][j]= -2 * self.dimension_coefficient * self.dimension_max
                 # Updating the position of the particle i
                 self.position[i] = self.position[i] + self.velocity[i]
-            # Recording the global best FF value and print it at each iteration.
-            fitness.append(self.gfit)
-            print(self.gfit)
+            # Recording the global best PE value and print it at each iteration.
+            time_cal_temp = time.time()
+
+            PE_fitness.append(self.gfit)
+            timer.append(time_cal_temp-time_cal_start)
+            self.gfit_temp = self.gfit   #Update the Temp fitness-value
 
         # When the program finished, print the final results.
         self.PP = self.gbest[0]
         self.xx0 = self.gbest[1]
         self.yy0 = self.gbest[2]
         print('The Global Best Position is ', self.gbest, self.PP, self.xx0, self.yy0)
-        return fitness
+        print('The gfit is', self.gfit)
+        return PE_fitness,timer
+
+
 
 if __name__=='__main__':
     # Checking the working directory and printing
@@ -173,7 +187,7 @@ if __name__=='__main__':
 
     # Matrixing the data and Generating the matrix A corresponding in the research paper.
     # The first column is the scan position data. The data in the second and third
-    # columns are the scanned lower profile Y_down and the upper profile Y_up.
+    # columns are the scanned down profile Y_down and the up profile Y_up.
     A = array(df[['Position', 'Data1', 'Data2']])
     print('The Matrix A is: \n',A)
 
@@ -186,13 +200,13 @@ if __name__=='__main__':
     # in this program, a represents the parameter M1 in the research paper.
     # in this program, b represents the parameter M2 in the research paper.
     # By manually adjusting a and b, we intercepted matrix B from matrix A.
-    a = 130
+    a = 140
     b = 210
     B = A[a:b, ]
 
     time_start = time.time()                # Recording the system time when the program starts.
     iter = 1000                             # Setting the value of iteration.
-    my_pso = PSO(50,3,iter,0.9,0.4,2,2,1e15,10000,0.2)     # Instantiating the 'PSO class' into my_pso.
+    my_pso = PSO(50,3,iter,0.9,0.4,2,2,1e15,1e15,10000,0.2)     # Instantiating the 'PSO class' into my_pso.
     # The number of particles were set to 50 and the dimension was 3;
     # the initial gfit was set to 1e15, dimension maximum was set to 10000, the coefficient was set to 0.2;
     # the inertia weight and acceleration factors were set.
@@ -201,7 +215,7 @@ if __name__=='__main__':
     my_pso.init_Population()
 
     # running the program
-    fitness = my_pso.iterator()
+    PE_fitness,timer = my_pso.iterator()
 
     # Obtaining the Matrix C
     C = my_pso.C_matrix()
@@ -211,46 +225,31 @@ if __name__=='__main__':
     print('total run cost is ',time_end-time_start)
 
     # Using matplotlib program package to draw the result figures.
-    # Drawing the curve diagram of  Global FF value and iterations.
+    # Drawing  the inner profile curves for the unequal diameter glass tube (Matrix A) and the
+    # parabolic lens found by PSO (Matrix C).
     plt.figure(1)
     plt.title("")
-    plt.xlabel("iteration", size=14)
-    plt.ylabel("FF of Gbest", size=14)
-    t = np.array([t for t in range(0, iter)])
-    fitness = np.array(fitness)
-    label_fitness, = plt.plot(t, fitness, color='b', linewidth=3)
-    plt.legend(handles=[label_fitness], labels=['FF of Gbest'], loc='upper right')
-    plt.savefig('FF of Gbest.png', dpi=300)
+    plt.xlabel("X-axis / mm", size=14)
+    plt.ylabel("Y-axis / mm", size=14)
+    label_A, = plt.plot(A[:,0],A[:,1], color='k',linewidth=4)
+    label_A, = plt.plot(A[:,0],A[:,2], color='k',linewidth=4)
+    label_c, = plt.plot(C[:,0],C[:,1], 'r|',linewidth=4,markersize=10)
+    label_c, = plt.plot(C[:,0],C[:,2], 'r|',linewidth=4,markersize=10)
+    plt.legend(handles=[label_A, label_c], labels=['Unequal diameter glass tube profile curve', 'Intercepted part profile curve'], loc='upper left')
+    plt.savefig('Intercepted part on the glass tube.png', dpi=600)
     plt.show()
 
     # Using matplotlib program package to draw the result figures.
-    # Drawing the details of the inner profile curves of the long lens in the intercepting area
-    # (Matrix B) and the parabolic lens in the intercepting area (Matrix C).
+    # Drawing the curve diagram of  Global FF value and calculate_time.
     plt.figure(2)
     plt.title("")
-    plt.xlabel("X-axis / mm", size=14)
-    plt.ylabel("Y-axis / mm", size=14)
-    label_B, = plt.plot(B[:,0],B[:,1], color='b',linewidth=3)
-    label_B, = plt.plot(B[:,0],B[:,2], color='b',linewidth=3)
-    label_C, = plt.plot(C[:,0],C[:,1], color='r',linewidth=3)
-    label_C, = plt.plot(C[:,0],C[:,2], color='r',linewidth=3)
-    plt.legend(handles = [label_B,label_C],labels= ['Real profile curve','Fitting profile curve'],loc = 'upper left')
-    plt.savefig('detail.png', dpi=300)
-    plt.show()
-
-    # Using matplotlib program package to draw the result figures.
-    # Drawing  the inner profile curves for the long lens (Matrix A) and the
-    # parabolic lens found by PSO (Matrix C).
-    plt.figure(3)
-    plt.title("")
-    plt.xlabel("X-axis / mm", size=14)
-    plt.ylabel("Y-axis / mm", size=14)
-    label_A, = plt.plot(A[:,0],A[:,1], color='b',linewidth=3)
-    label_A, = plt.plot(A[:,0],A[:,2], color='b',linewidth=3)
-    label_c, = plt.plot(C[:,0],C[:,1], color='r',linewidth=3)
-    label_c, = plt.plot(C[:,0],C[:,2], color='r',linewidth=3)
-    plt.legend(handles=[label_A, label_c], labels=['Real all profile curve', 'Fitting profile curve'], loc='upper left')
-    plt.savefig('whole.png', dpi=300)
+    plt.xlabel("Run time / s", size=14)
+    plt.ylabel("PE / mm", size=14)
+    timer = np.array(timer)
+    PE_fitness = np.array(PE_fitness)
+    label_fitness, = plt.plot(timer, PE_fitness, color='b', linewidth=4)
+    plt.legend(handles=[label_fitness], labels=['Global best PE value'], loc='upper right')
+    plt.savefig('PE value with time.png', dpi=600)
     plt.show()
 
     #Saving the results to Files
